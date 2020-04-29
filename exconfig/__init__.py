217ce97cd8724a37e6e2aaba645f5d28196810e9
mcflugen <mcflugen@gmail.com>
2020-04-29 17:02:09 -0600

    make pretty

--- exconfig/__init__.py
+++ exconfig/__init__.py
@@ -10,7 +10,16 @@ from .field import (
     IntegerField,
 )
 
-__all__ = ["Configuration", "validators", "ValidationError", "ArrayField", "BooleanField", "FloatField", "IntegerField", "ConfigurationField"]
+__all__ = [
+    "ArrayField",
+    "BooleanField",
+    "Configuration",
+    "ConfigurationField",
+    "FloatField",
+    "IntegerField",
+    "ValidationError",
+    "validators",
+]
 
 __version__ = get_versions()["version"]
 del get_versions
